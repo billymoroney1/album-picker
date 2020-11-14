febc091ee5a0d3c31cad142c14c88d91628b7f38
billymoroney <YOUR-EMAIL-ADDRESS>
2020-11-14 09:03:35 -0600

    right before presenting

--- public/app.js
+++ public/app.js
@@ -3,3 +3,17 @@ const body = document.querySelector('body')
 body.addEventListener('click', () => {
     console.log('body clicked')
 })
+
+
+M.AutoInit()
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
